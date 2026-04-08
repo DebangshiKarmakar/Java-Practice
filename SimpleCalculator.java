1958df1ac5e4f9b432e79eccc1f166aa4c1961e7
Debangshi Karmakar <codebot42@gmail.com>
2026-04-08 12:32:12 +0530

    Add Calculator

---
+++ SimpleCalculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1= sc.nextDouble();
+
+        System.out.print("Enter the operator(+,-,*,/): ");
+        char operator= sc.next().charAt(0); // grabs just the first position of the operator entered
+
+        System.out.print("Enter second number: ");
+        double num2= sc.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result= num1 + num2;
+                break;
+        
+            case '-':
+                result=num1 - num2;
+                break;
+            
+            case '*':
+                result= num1 * num2;
+                break;
+            case '/':
+                if(num2 !=0){
+                    result= num1/num2;
+
+                }
+                else{
+                    System.out.println("Error: Division by zero is NOT ALLOWED!");
+                    sc.close();
+                    return;
+
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator");
+                sc.close();
+                return;
+
+        }
+    System.out.println("Result: " + result);
+    sc.close();
+
+
+
+    }
+}
